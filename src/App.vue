f6aa7696efe7973e150d5fe920d461d2b871a457
方向明 <fangxiangming@fang-Mac.local>
2022-05-21 00:57:31 +0800

    feat:storage封装

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <script>
 export default {
-  name: 'app'
+  name: 'app',
+  mounted() {
+    this.$storage.setItem('user', { name: 'fang', age: '22' })
+    this.$storage.setItem('type', 'human')
+    console.log('user =>', this.$storage.getItem('user'))
+    console.log('storage =>', this.$storage.getStorage())
+    this.$storage.clearItem('user')
+    console.log('delete storage =>', this.$storage.getStorage())
+  }
 }
 </script>
 
--- src/config/index.js
+++ src/config/index.js
@@ -20,9 +20,9 @@ const EnvConfig = {
 }
 
 
-
 export default {
     env,
     mock: true,
+    namespace: 'vue3Koa2',
     ...EnvConfig[env]
 }
