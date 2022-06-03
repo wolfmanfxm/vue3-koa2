08a78d23d66cb390428d41bf437be5f6f2a6f01b
方向明 <fangxiangming@fang-Mac.local>
2022-06-03 21:55:01 +0800

    login页面交互

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,5 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import Home from './../components/Home.vue';
-import Welcome from './../components/Welcome.vue';
-import Login from './../components/Login.vue';
-
 
 
 const routes = [
@@ -21,17 +18,18 @@ const routes = [
                 meta: {
                     title: '欢迎'
                 },
-                component: Welcome,
-            },
-            {
-                name: 'login',
-                path: '/login',
-                meta: {
-                    title: '登陆'
-                },
-                component: Login,
+                component: () => import('./../views/Welcome.vue'),
             }
         ]
+    },
+    {
+        name: 'login',
+        path: '/login',
+        meta: {
+            title: '登陆'
+        },
+        component: () => import('./../views/Login.vue')
+
     }
 ]
 
